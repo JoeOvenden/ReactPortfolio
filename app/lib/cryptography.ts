2f038c4110c42e96e7cb112b9ad4b3d589a1a642
Joe Ovenden <joe.ovenden17@gmail.com>
2026-01-19 13:33:35 +0000

    removed unused file

--- app/lib/cryptography.ts
+++ app/lib/cryptography.ts
@@ -1,35 +1,27 @@
-import { sha256 } from "js-sha256";
-import '@/envConfig.ts';
-import { PasswordData } from "./definitions/User";
-import { hash } from "crypto";
+import { sha256 } from 'js-sha256';
 
 export function verifyPassword(
-  salt: string,
-  password: string,
-  hash: string,
-  rounds?: number,
-  hashingFunction?: 'sha256'
+    salt: string,
+    password: string,
+    hash: string,
+    rounds?: number,
+    hashingFunction?: 'sha256',
 ) {
     return hashPassword(salt, password) === hash;
 }
 
-export function hashPassword(
-  salt: string,
-  password: string,
-  rounds?: number,
-  hashingFunction?: 'sha256'
-) {
+export function hashPassword(salt: string, password: string, rounds?: number, hashingFunction?: 'sha256') {
     if (salt.length < 16) {
-        throw new Error("Salt too short");
+        throw new Error('Salt too short');
     }
-    const pepper = process.env.PASSWORD_PEPPER ?? "";
+    const pepper = process.env.PASSWORD_PEPPER ?? '';
     if (pepper.length < 16) {
-        throw new Error("Pepper is either null or too short");
+        throw new Error('Pepper is either null or too short');
     }
 
     rounds = rounds ?? 10000;
     let hash = password;
-    for(let i = 0; i < rounds; i++) {
+    for (let i = 0; i < rounds; i++) {
         hash = sha256(salt + hash + pepper);
     }
     return hash;
